3cb8fc17f7cde60613483a553d1b7bbe045939e8
Vijasan Vasantharajan <vijasan.v@hotmail.com>
2023-02-02 11:57:35 +0100

    Javascript + added retry button

---
+++ rockpaperscissors/script.js
@@ -0,0 +1,47 @@
+const options = ["rock", "paper", "scissors"];
+const buttons = document.querySelectorAll("button"); // select all buttons
+const player1 = document.querySelector("#player1"); // select player 1
+const player2 = document.querySelector("#player2"); // select player 2
+const lose = document.querySelector("#lose"); // select the lose result
+const win = document.querySelector("#win"); // select the win result
+const draw = document.querySelector("#draw"); // select the draw result
+
+buttons.forEach((button) => {
+  button.addEventListener("click", function () {
+    const playerChoice = this.className; // get the player's choice
+    const computerChoice = options[Math.floor(Math.random() * options.length)]; // generate computer's choice
+    player1.className = `player ${playerChoice} shake`; // update player 1's class
+    player2.className = `player ${computerChoice} shake`; // update player 2's class
+
+    if (playerChoice === computerChoice) {
+      draw.classList.remove("hidden"); // show draw result
+    } else if (
+      (playerChoice === "rock" && computerChoice === "scissors") ||
+      (playerChoice === "paper" && computerChoice === "rock") ||
+      (playerChoice === "scissors" && computerChoice === "paper")
+    ) {
+      win.classList.remove("hidden"); // show win result
+    } else {
+      lose.classList.remove("hidden"); // show lose result
+    }
+
+    setTimeout(function () {
+      player1.classList.remove("shake"); // remove shake animation from player 1
+      player2.classList.remove("shake"); // remove shake animation from player 2
+    }, 500);
+  });
+});
+
+// adds retry button
+const retryButton = document.createElement("button");
+retryButton.innerHTML = "Retry";
+document.body.appendChild(retryButton);
+
+retryButton.addEventListener("click", function () {
+  retryButton.classList.add("retry-button");
+  win.classList.add("hidden");
+  lose.classList.add("hidden");
+  draw.classList.add("hidden");
+  player1.className = "player";
+  player2.className = "player";
+});
